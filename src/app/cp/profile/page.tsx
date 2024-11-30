fdf9303c81bea335fc208257ae27342e7029a256
Cagatay Uslu <cagatay.uslu@code.berlin>
2024-11-30 05:21:46 +0100

    add profile pages

---
+++ src/app/cp/profile/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = () => {
+  return <div>profile</div>;
+};
+
+export default page;
---
+++ src/app/cp/profile/settings/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = () => {
+  return <div>settings</div>;
+};
+
+export default page;
