0737aed5859c9ee2fdd9d7e22aab3e2202cd13b2
Cagatay Uslu <cagatay.uslu@code.berlin>
2024-12-07 00:45:29 +0100

    add posts feed to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar";
+import PostsWithDetails from "./components/PostsFeed";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div>home</div>
+      <PostsWithDetails />
     </>
   );
 }
